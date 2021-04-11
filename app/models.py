caad26c4c5c90a4d6f907aa4d068973744d99f43
Krishnanunni R <krishnanunnir97@gmail.com>
2021-04-11 20:04:55 +0530

    [refactor] Changed model variable name to PEP8 and some DB changes
    Changed 1 to success and 0 to failure
    Changed imported_on a DateTime field to imported_date a
    Date field
    Added import_details foreign key

--- app/models.py
+++ app/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
 STATUS = (
-    (0, 'SUCCESS'),
-    (1, 'FAILURE'),
+    (1, 'SUCCESS'),
+    (0, 'FAILURE'),
 )
 # Create your models here.
 class ImportDetails(models.Model):
     """
     Details about the import and its status
     """
-    imported_on = models.DateTimeField(auto_now= True)
+    imported_date = models.DateField(auto_now= True)
     status = models.IntegerField(choices= STATUS, default= 0)
 class ImportDate(models.Model):
     """
     Data about equity added to this db
     """
     # Storing as CharField as we won't use as number.
-    CODE = models.CharField(max_length= 6)
-    NAME = models.CharField(max_length= 26)
+    code = models.CharField(max_length= 6)
+    name = models.CharField(max_length= 26)
     # Storing as Integer for future usage
-    OPEN = models.IntegerField()
-    HIGH = models.IntegerField()
-    LOW = models.IntegerField()
-    CLOSE = models.IntegerField()
+    open_value = models.IntegerField()
+    high_value = models.IntegerField()
+    low_value = models.IntegerField()
+    close_value = models.IntegerField()
+    import_details = models.ForeignKey(ImportDetails,on_delete= models.CASCADE)
