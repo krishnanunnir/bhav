f49371297767bd1d1abfb070869ef1e5e4615313
Krishnanunni R <krishnanunnir97@gmail.com>
2021-04-18 00:44:40 +0530

    [feature] Added view to render api for stockname

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from app import views
 
+# using str for string pattern instead of slug as slug doesn't allow spaces
 urlpatterns = [
     path('', views.stock_list),
+    path('<str:stockname>', views.stock_list_by_name),
 ]
--- app/views.py
+++ app/views.py
@@ -7,7 +7,21 @@ from django.http import HttpResponse, JsonResponse
 
 @csrf_exempt
 def stock_list(request):
+    """
+    Returns an API with content of all the stocks
+    since there is only one method, I don't think there is need for abstracts and such like viewsets
+    """
     if request.method=="GET":
         importDateObj = ImportDate.objects.all()
+        serializer = ImportDateSerializer(importDateObj,many= True)
+        return JsonResponse(serializer.data,safe= False)
+
+def stock_list_by_name(request, stockname):
+    """
+    Returns an API with content of the stock with name stockname
+    Search is basic query with sql 'like', so not as accurate
+    """
+    if request.method=="GET":
+        importDateObj = ImportDate.objects.filter(name__icontains=stockname)
         serializer = ImportDateSerializer(importDateObj,many= True)
         return JsonResponse(serializer.data,safe= False)
