e95bb745f3bde0f29dd9eacd2c3ead2064bf6886
Krishnanunni R <krishnanunnir97@gmail.com>
2021-04-18 17:43:29 +0530

    Revert "[feature] Added pagination for the data"
    This reverts commit 910553215d2dd28dae44e42ae2432f4321731cae.

--- app/views.py
+++ app/views.py
@@ -3,36 +3,27 @@ from .serializers import ImportDateSerializer
 from django.views.decorators.csrf import csrf_exempt
 from .models import ImportDate
 from django.http import HttpResponse, JsonResponse
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import api_view
 # Create your views here.
 
-@api_view(['GET'])
+@csrf_exempt
 def stock_list(request):
     """
     Returns an API with content of all the stocks
     since there is only one method, I don't think there is need for abstracts and such like viewsets
     """
     if request.method=="GET":
-        paginator = PageNumberPagination()
-        paginator.page_size = 50
         importDateObj = ImportDate.objects.all().order_by("-id")
-        result_page = paginator.paginate_queryset(importDateObj, request)
-        serializer = ImportDateSerializer(result_page,many= True)
+        serializer = ImportDateSerializer(importDateObj,many= True)
         return JsonResponse(serializer.data,safe= False)
 
-@api_view(['GET'])
 def stock_list_by_name(request, stockname):
     """
     Returns an API with content of the stock with name stockname
     Search is basic query with sql 'like', so not as accurate
     """
     if request.method=="GET":
-        paginator = PageNumberPagination()
-        paginator.page_size = 50
-        importDateObj = ImportDate.objects.filter(name__icontains=stockname)
-        result_page = paginator.paginate_queryset(importDateObj, request)
-        serializer = ImportDateSerializer(result_page,many= True)
+        importDateObj = ImportDate.objects.filter(name__icontains=stockname).order_by("-id")
+        serializer = ImportDateSerializer(importDateObj,many= True)
         return JsonResponse(serializer.data,safe= False)
 
 def render_stock(request, template = "index.html"):
