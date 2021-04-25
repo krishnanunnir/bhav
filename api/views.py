117ea6afe26039c6631805904c0df43a00989340
Krishnanunni R <krishnanunnir97@gmail.com>
2021-04-25 11:51:51 +0530

    [documentation] added some comments to better explain the apis

--- api/views.py
+++ api/views.py
@@ -4,21 +4,23 @@ from django.core.cache import cache
 from .serializers import EquitySerializer
 from .Pagination import PageNumberCountPagination
 from rest_framework.decorators import api_view
-# Create your views here.
 
 @api_view(['GET'])
 def stock_list(request):
     """
-    Returns an API with content of all the stocks
+    API returning all the contents of cached data
     since there is only one method, I don't think there is need for abstracts and such like viewsets
     """
     if request.method=="GET":
+        # paginating with our own paginator for future extensability
         paginator = PageNumberCountPagination()
         paginator.page_size = 200
         all_stocks = []
         # keys are not ideal in very large caches
-        # but our db at max contains 3000 records corresponding to equity csv
+        # but our cache at max contains 4000 records corresponding to equity csv
+        # wildcards can be used with cache keys
         stock_list =  cache.keys("*")
+        # using the stock list to retrieve the values from cache
         # excluding the latest value in cache which contains the last day it was updated
         for stock in [x for x in stock_list if x!="latest"]:
             all_stocks+=[cache.get(stock)]
@@ -29,13 +31,13 @@ def stock_list(request):
 @api_view(['GET'])
 def stock_list_by_name(request, stockname):
     """
-    Returns an API with content of the stock with name stockname
-    Search is basic query with sql 'like', so not as accurate
+    API returning the all the stocks containing the stockname pattern
     """
     if request.method=="GET":
         paginator = PageNumberCountPagination()
         paginator.page_size = 200
         search_result = []
+        # very similar to stock_list method, in cache keys we are adding the name of stock we are searching for
         search_key = cache.keys("*"+stockname.lower() +"*")
         for key in [x for x in search_key if x!="latest"]:
             search_result+=[cache.get(key)]
