3f738fcf11a391b1cd95ce3fddcf669c9c95319f
Krishnanunni R <krishnanunnir97@gmail.com>
2021-04-25 18:05:55 +0530

    [fix] Updated timeout to never expire until cleared

--- bhavcopy/settings.py
+++ bhavcopy/settings.py
@@ -134,6 +134,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379/',
+        'TIMEOUT': None,
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
